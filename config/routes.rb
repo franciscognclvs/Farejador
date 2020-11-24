41493c73ed0b9166dc3c4a9234838b5df7bfccaa
afonso brandao <afonsolelis@gmail.com>
2020-11-24 11:24:42 -0300

    modificacao nos controllers do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'animals#index'
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
+  root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :adoption
