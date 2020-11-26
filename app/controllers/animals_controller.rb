f289fb681907ddf144d72af7de2ebbe3bba5cdad
francisco goncalves <fgp1991@gmail.com>
2020-11-26 15:26:12 -0300

    index mostrando só animals para adoção

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,8 +3,7 @@ class AnimalsController < ApplicationController
   before_action :set_animal, only: %i[show edit update destroy]
 
   def index
-    # @animals = Animal.where(owner: current_user)
-    @animals = Animal.where(adopted: false)
+    @animals = Animal.where(adopted: false, owner: current_user)
   end
 
   def show
